83f855161d1d125a04c8e2c1646126d541f09892
r4deadv <mihovilovic.rade@gmail.com>
2022-01-16 19:02:56 +0100

    logic added -create grid -reset grid -color cells -removing color from cells

--- designs.js
+++ designs.js
@@ -1,10 +1,42 @@
-// Select color input
-// Select size input
+//User input variable
+const inputHeight = document.getElementById("inputHeight");
+const inputWidth = document.getElementById("inputWidth");
+const colorPicker = document.getElementById("colorPicker");
+const button = document.getElementById("button");
 
-// When size is submitted by the user, call makeGrid()
+//Grid variable
+const pixelCanvas = document.getElementById("pixelCanvas");
 
+//Making grid function
 function makeGrid() {
-
-// Your code goes here!
-
+  //Reseting grid
+  pixelCanvas.innerHTML = "";
+  //Creating grid loop
+  for (let i = 0; i < inputHeight.value; i++) {
+    let gridRow = document.createElement("tr");
+    pixelCanvas.appendChild(gridRow);
+    for (let j = 0; j < inputWidth.value; j++) {
+      let gridCell = document.createElement("td");
+      gridRow.appendChild(gridCell);
+      //Coloring logic
+      gridCell.addEventListener("click", function (e) {
+        console.log(`pointer event x${e.x} y${e.y}`);
+        console.log(colorPicker.value);
+        if (gridCell.hasAttribute("style")) {
+          e.target.removeAttribute("style");
+        } else {
+          e.target.style.backgroundColor = colorPicker.value;
+        }
+      });
+    }
+  }
+  console.log(`creating grid`);
 }
+
+//submit button click event listener
+button.addEventListener("click", function (click) {
+  click.preventDefault();
+  makeGrid();
+  colorPicker.value = "#000000";
+  button.value = "Reset";
+});
